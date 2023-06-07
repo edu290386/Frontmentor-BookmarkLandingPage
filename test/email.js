5b9bb64c03a62d772c880de8cc712623fe1e7d4d
edu290386 <eduardo.torres@outlook.com>
2023-06-06 19:05:19 -0500

    header correction

--- test/email.js
+++ test/email.js
@@ -1,15 +1,14 @@
-const emailInput = document.querySelector('.emailInput');
-
-emailInput.addEventListener('input', () => {
-  const email = emailInput.value;
-
-  // Regular expression to check if the email address is valid
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  // Check if the email address is valid
-  if (regex.test(email)) {
-    document.querySelector('.invalid::after').style.color = 'green';
-  } else {
-    document.querySelector('.invalid::after').style.color = 'red';
-  }
-});
+const valor1 = parseFloat(prompt("Primer valor"));
+const valor2 = parseFloat(prompt("Segundo valor"));
+const operacion = prompt("Ingrese operacion: +,-,x,/");
+let resultado = 0;
+if (operacion ==="+"){
+  resultado = valor1 + valor2;
+} else if (operacion === "-"){
+  resultado = valor1 - valor2;
+} else if (operacion === "x"){
+  resultado = valor1*valor2;
+} else {
+  resultado = valor1/valor2;
+}
+console.log(resultado);
