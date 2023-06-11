38f4abb2b2bc2b867702320d57fb94099ee4597c
edu290386 <eduardo.torres@outlook.com>
2023-06-11 17:52:51 -0500

    menu responsive

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav-menu");
 const exit = document.querySelector(".exit");
-console.log(hamburger);
-console.log(nav);
+const header = document.querySelector("header");
+const logo = document.querySelector(".logomaster");
+const li = document.querySelector("li");
 
 hamburger.addEventListener("click", () => {
-    console.log("xxxxxxxx")
     nav.classList.toggle("showmenu");
-    
+    header.style.backgroundColor = "#2a2d47";
+    hamburger.style.display = "none";
+    exit.style.display = "block";
+    logo.style.filter = "invert(100%) sepia(100%) saturate(0%) hue-rotate(21deg) brightness(97%) contrast(103%)";
 });
+
+exit.addEventListener("click", () => {
+    hamburger.style.display = "block";
+    exit.style.display = "none";
+    nav.classList.toggle("showmenu");
+    header.style.backgroundColor = "#FFFFFF";
+    logo.style.filter = "none";
+});
+
