17824037b4e6b7128ec3c54a7f797f3e7bdc6613
edu290386 <eduardo.torres@outlook.com>
2023-06-11 19:17:36 -0500

    feature ended

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const nav = document.querySelector(".nav-menu");
 const exit = document.querySelector(".exit");
 const header = document.querySelector("header");
 const logo = document.querySelector(".logomaster");
-const li = document.querySelector("li");
+const feature1 = document.querySelector(".first-button");
+const img1feature = document.querySelector("#img1st-feature")
+const art1feature = document.querySelector("#art1st-feature")
+const feature2 = document.querySelector(".second-buttons");
+const img2feature = document.querySelector("#img2nd-feature")
+const art2feature = document.querySelector("#art2nd-feature")
+const feature3 = document.querySelector(".last-buttons");
+const img3feature = document.querySelector("#img3rd-feature")
+const art3feature = document.querySelector("#art3rd-feature")
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("showmenu");
@@ -21,3 +29,29 @@ exit.addEventListener("click", () => {
     logo.style.filter = "none";
 });
 
+feature1.addEventListener("click", () => {
+    img1feature.style.display = "block";
+    art1feature.style.display = "block";
+    img3feature.style.display = "none";
+    art3feature.style.display = "none";
+    img2feature.style.display = "none";
+    art2feature.style.display = "none";
+});
+
+feature2.addEventListener("click", () => {
+    img1feature.style.display = "none";
+    art1feature.style.display = "none";
+    img3feature.style.display = "none";
+    art3feature.style.display = "none";
+    img2feature.style.display = "block";
+    art2feature.style.display = "block";
+});
+
+feature3.addEventListener("click", () => {
+    img1feature.style.display = "none";
+    art1feature.style.display = "none";
+    img3feature.style.display = "block";
+    art3feature.style.display = "block";
+    img2feature.style.display = "none";
+    art2feature.style.display = "none";
+});
